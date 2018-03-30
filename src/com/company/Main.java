19072457ac5c984e689494b68827ef0a6152fe21
Eden Bereda <edukenat@gmail.com>
2018-03-30 13:36:05 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter n:");
+        int n = scan.nextInt();
+        scan.nextLine();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+
+
+                System.out.println("FizzBuzz");
+
+            } else if (i % 5 == 0) {
+
+                System.out.println("Buzz");
+
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+
+
+            //FIZZBUZZ BONUS
+            {
+                if (i % 7 == 0 && i % 17 == 0 && i % 23 == 0)
+                    System.out.println("Multipel of 7,17,23");
+
+            }
+
+            if (i % 7 == 0) {
+                System.out.println("Multipel of 7");
+
+            } else if (i % 17 == 0) {
+                System.out.println("Multipel of 17");
+
+            } else if (i % 23 == 0) {
+                System.out.println("Multipel of 23");
+
+            } else {
+                System.out.println(i);
+            }
+
+        }
+    }
+
+}
+
+
+
+
+
